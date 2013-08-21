d9b43f1163e450236a5b0e6b20d79ea2e775ac53
danfengchaoxi <871279061@qq.com>
2013-08-21 21:07:41 +0800

    Update quote.js

--- quote.js
+++ quote.js
@@ -23,15 +23,7 @@ if("undefined"!=typeof Bookmark){
 		    a=d.firstChild;
 		    b.appendChild(a);
 	    } 
-	    var addEvent=function(a,b,d){
-	        alert('addevent');
-			if(window.addEventListener){
-		        a.addEventListener(b,d,false);
-		    }else{
-		        a.attachEvent("on"+b,d);
-		    }
-	    }
-		
+	    		
 		var o = {
 		        create:function(){
 				    return function(){
@@ -43,22 +35,15 @@ if("undefined"!=typeof Bookmark){
 		wnd.prototype={
 			initialize:function(){
 			    this.createWnds();
-				this.addAllEvents();
 			},
-			createWnds:function(){//建主窗口
+			createWnds:function(){
 				alert('createwnds');
-				var a='<div id="idBookmarkMainWnd" class="cssBookmark" style="border:2px solid black;width:100px;height:100px;border:1px solid black;background:#FF0000;"><div id="idBookmarkPopupToolbar"><div title="关闭" id="idBookmarkClose"></div></div><div id="idBookmarkCtrlWnd"><form id="idBookmarkForm" method="get" name="idBookmarkForm"><table style="width:300px;height:33px;" height="33px" cellspacing="0" cellpadding="0" border="0"><tr><td style="width:55px;height:33px;vertical-align:top;" width="55px"><input style="margin-top:4px;" id="idBookmarkQuerySubmit" type="submit" value="" click="" hidefocus="true" onFocus="this.blur()" /></td></tr></table></form></div><div id="idBookmarkContentWnd"></div><div id="idBookmarkFooterWnd">foot</div></div>';
+				var a='<div id="idBookmarkMainWnd" class="cssBookmark" style="border:2px solid black;background:#FF0000;"><div id="idBookmarkPopupToolbar"><div title="关闭" id="idBookmarkClose"></div></div><div id="idBookmarkCtrlWnd"><form id="idBookmarkForm" method="get" name="idBookmarkForm"><table style="width:300px;height:33px;" height="33px" cellspacing="0" cellpadding="0" border="0"><tr><td style="width:55px;height:33px;vertical-align:top;" width="55px"><input style="margin-top:4px;" id="idBookmarkQuerySubmit" type="submit" value="" click="" hidefocus="true" onFocus="this.blur()" /></td></tr></table></form></div><div id="idBookmarkContentWnd"></div><div id="idBookmarkFooterWnd">foot</div></div>';
                 addElement(a,document.body);
 				this.mainWnd=c("idBookmarkMainWnd");
 		        this.mainWnd.style.position="fixed";
 		        this.mainWnd.style.left=200+'px';
 		        this.mainWnd.style.top=300+'px';		
-			},
-			addAllEvents:function(){//增加事件
-				this.onClose=function(){
-				    this.mainWnd.style.visibility="hidden";
-				}
-				addEvent(c("idBookmarkClose"),"click",this.onClose);
 			}
 		};
 		Bookmark.oMainWnd=new wnd();		
