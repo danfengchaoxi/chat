54adb897a6a41288140a9a9dc9de49732e468b1e
danfengchaoxi <871279061@qq.com>
2013-08-21 19:53:18 +0800

    Update quote.js

--- quote.js
+++ quote.js
@@ -3,19 +3,19 @@ if("undefined"!=typeof Bookmark){
 	    Bookmark.oMainWnd.mainWnd.style.visibility="visible";
 	}
 }else{
-    alert('else');
-	var Bookmark={};
+    var Bookmark={};
 	(function(){	    
-		alert('function');
 		function c(a){//获取ID
-	        if(typeof(a)=="string"){
+	        alert('c');
+			if(typeof(a)=="string"){
 		        return document.getElementById(a);
 		    }else{
 		        return a;
 			}
 	    }
 	    var addElement=function(a,b){
-	        if(undefined===b){
+	        alert('addelement');
+			if(undefined===b){
 		        b=document.body;
 		    }
 		    var d=document.createElement("div");
@@ -24,7 +24,8 @@ if("undefined"!=typeof Bookmark){
 		    b.appendChild(a);
 	    } 
 	    var addEvent=function(a,b,d){
-	        if(window.addEventListener){
+	        alert('addevent');
+			if(window.addEventListener){
 		        a.addEventListener(b,d,false);
 		    }else{
 		        a.attachEvent("on"+b,d);
@@ -46,8 +47,8 @@ if("undefined"!=typeof Bookmark){
 			},
 			createWnds:function(){//建主窗口
 				alert('createwnds');
-				var a='<div id="idBookmarkMainWnd" class="cssBookmark" style="border:2px solid black;"><div id="idBookmarkPopupToolbar"><div title="关闭" id="idBookmarkClose"></div></div><div id="idBookmarkCtrlWnd"><form id="idBookmarkForm" method="get" name="idBookmarkForm"><table style="width:300px;height:33px;" height="33px" cellspacing="0" cellpadding="0" border="0"><tr><td style="width:55px;height:33px;vertical-align:top;" width="55px"><input style="margin-top:4px;" id="idBookmarkQuerySubmit" type="submit" value="" click="" hidefocus="true" onFocus="this.blur()" /></td></tr></table></form></div><div id="idBookmarkContentWnd"></div><div id="idBookmarkFooterWnd">foot</div></div>';
-		        addElement(a,document.body);
+				var a='<div id="idBookmarkMainWnd" style="width:100px;height:100px;border:1px solid black;"><p>nihao</p></div>';
+				addElement(a,document.body);
 		        this.mainWnd=c("idBookmarkMainWnd");
 		        this.mainWnd.style.position="fixed";
 		        this.mainWnd.style.left=200+'px';
