c60220d69a22c3eeb7df1a342eb133a38f60cf0c
danfengchaoxi <871279061@qq.com>
2013-09-02 21:20:07 +0800

    Update quote.js

--- quote.js
+++ quote.js
@@ -6,8 +6,7 @@ if("undefined"!=typeof Bookmark){
     var Bookmark={};
 	(function(){	    
 		function c(a){//获取ID
-	        alert('c');
-			if(typeof(a)=="string"){
+	        if(typeof(a)=="string"){
 		        return document.getElementById(a);
 		    }else{
 		        return a;
@@ -56,13 +55,86 @@ if("undefined"!=typeof Bookmark){
 			},
 			addAllEvents:function(){//增加事件
 				this.onClose=function(){
-				    alert('close');
 					var mainWnd = c("idBookmarkMainWnd");
 					mainWnd.style.visibility="hidden";
 				}
 				addEvent(c("idBookmarkClose"),"click",this.onClose);
+				
 			}
 		};
-		Bookmark.oMainWnd=new wnd();		
+		
+		
+funs={
+    index:100,  
+    getFocus:function (target){  
+        if(target.style.zIndex!=this.index){  
+            this.index += 2;  
+            var idx = this.index;  
+            target.style.zIndex=idx;  
+        }  
+    },
+    abs:function (element) {
+        var result = { x: element.offsetLeft, y: element.offsetTop};
+        element = element.offsetParent;
+        while (element) {
+            result.x += element.offsetLeft;
+            result.y += element.offsetTop;
+            element = element.offsetParent;
+        }
+        return result;
+    }
+};
+
+function Endrag(source,target,offSetX, offSetY){
+    source=typeof(source)=="object" ? source:document.getElementById(source);
+    target=typeof(target)=="object" ? target:document.getElementById(target);
+    var x0=0,y0=0,x1=0,y1=0,moveable=false,index=100,NS=(navigator.appName=='Netscape');
+    offSetX=typeof offSetX== "undefined" ? 0:offSetX;
+    offSetY=typeof offSetY== "undefined" ? 0:offSetY;
+    source.onmousedown=function(e){
+        e = e ? e : (window.event ? window.event : null);
+        funs.getFocus(target); 
+        if(e.button==(NS)?0 :1)  { 
+            if(!NS){this.setCapture()}
+            x0 = e.clientX ;  
+            y0 = e.clientY ;  
+            x1 = parseInt(funs.abs(target).x);  
+            y1 = parseInt(funs.abs(target).y);    
+            moveable = true;  
+        }  
+    };  
+    //拖动;  
+    source.onmousemove=function(e){
+        e = e ? e : (window.event ? window.event : null);  
+        if(moveable){  
+            target.style.left = (x1 + e.clientX - x0 - offSetX) + "px";  
+            target.style.top  = (y1 + e.clientY - y0 - offSetY) + "px";  
+        }  
+    }; 
+    //停止拖动;  
+    source.onmouseup=function (e){ 
+        if(moveable)  {   
+            if(!NS){this.releaseCapture();}
+            moveable = false;  
+        }  
+    };
+}
+		
+		var moveWnd = o.create();
+		moveWnd.prototype = {
+		    initialize:function(a,b){
+			    
+				this.draggedElement=c(a);//拖拽元素，MainWnd
+				this.forcePointElement=c(b);//施力点 ，Toolbar
+				this.strOldStylePostion=this.draggedElement.style.position;//原position
+				this.forcePointElement.style.cursor="move";//鼠标形状
+				Endrag(b,a,0,0);	
+			}
+			//addEvent(c("idBookmarkClose"),"click",this.onClose);
+		};
+		
+		
+		Bookmark.oMainWnd=new wnd();	
+        Bookmark.oDrag = new moveWnd("idBookmarkMainWnd","idBookmarkPopupToolbar");		
     })()
 }
